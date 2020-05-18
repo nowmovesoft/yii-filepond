0a7f9a0a2c82f335f44bc60cf50badc254326008
Michael Naumov <vommuan@gmail.com>
2020-05-18 11:45:28 +0700

    FileValidator works as adapter

--- src/validators/FileValidator.php
+++ src/validators/FileValidator.php
@@ -2,10 +2,11 @@
 
 namespace nms\filepond\validators;
 
-use yii\validators\ValidationAsset;
-
 /**
- * Validates FilePond instance.
+ * Client validation rules are integrated in FilePond instance.
+ * Server validation rules are integrated in `nms\filepond\models\File` model.
+ * It isn't needed to validate rules here. This class is used only as adapter
+ * for components, which are specified above.
  * @author Michael Naumov <vommuan@gmail.com>
  */
 class FileValidator extends \yii\validators\FileValidator
@@ -13,35 +14,22 @@ class FileValidator extends \yii\validators\FileValidator
     /**
      * {@inheritdoc}
      */
-    public function clientValidateAttribute($model, $attribute, $view)
+    public function validateAttributes($model, $attributes = null)
     {
-        ValidationAsset::register($view);
-        $options = $this->getClientOptions($model, $attribute);
-
-        return 'yii.validation.string(value, messages, ' . json_encode($options, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . ');';
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getClientOptions($model, $attribute)
+    public function validateAttribute($model, $attribute)
     {
-        $label = $model->getAttributeLabel($attribute);
+    }
 
-        $options = [
-            'message' => $this->formatMessage($this->message, [
-                'attribute' => $label,
-            ]),
-        ];
-
-        $options['skipOnEmpty'] = $this->skipOnEmpty;
-
-        if (!$this->skipOnEmpty) {
-            $options['uploadRequired'] = $this->formatMessage($this->uploadRequired, [
-                'attribute' => $label,
-            ]);
-        }
-
-        return $options;
+    /**
+     * {@inheritdoc}
+     */
+    public function clientValidateAttribute($model, $attribute, $view)
+    {
+        return null;
     }
 }
