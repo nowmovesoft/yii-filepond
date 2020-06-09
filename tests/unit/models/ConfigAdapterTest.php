5148c23ac1bf62e900b5b8371e9cc525a7717c0c
Michael Naumov <vommuan@gmail.com>
2020-06-09 13:28:32 +0700

    ConfigAdapter tests. Simplify tests for addMessages() method.

--- tests/unit/models/ConfigAdapterTest.php
+++ tests/unit/models/ConfigAdapterTest.php
@@ -372,83 +372,58 @@ class ConfigAdapterTest extends \Codeception\Test\Unit
         $this->assertArrayNotHasKey('maxFiles', $adapter->filePond);
     }
 
-    public function testAddMessages()
+    public function addMessageProvider()
     {
+        return [
+            ['labelIdle', 'text-1'],
+            ['labelInvalidField', 'text-2'],
+            ['labelFileWaitingForSize', 'text-3'],
+            ['labelFileSizeNotAvailable', 'text-4'],
+            ['labelFileLoading', 'text-5'],
+            ['labelFileLoadError', 'text-6'],
+            ['labelFileProcessing', 'text-7'],
+            ['labelFileProcessingComplete', 'text-8'],
+            ['labelFileProcessingAborted', 'text-9'],
+            ['labelFileProcessingError', 'text-10'],
+            ['labelFileProcessingRevertError', 'text-11'],
+            ['labelFileRemoveError', 'text-12'],
+            ['labelTapToCancel', 'text-13'],
+            ['labelTapToRetry', 'text-14'],
+            ['labelTapToUndo', 'text-15'],
+            ['labelButtonRemoveItem', 'text-16'],
+            ['labelButtonAbortItemLoad', 'text-17'],
+            ['labelButtonRetryItemLoad', 'text-18'],
+            ['labelButtonAbortItemProcessing', 'text-19'],
+            ['labelButtonUndoItemProcessing', 'text-20'],
+            ['labelButtonRetryItemProcessing', 'text-21'],
+            ['labelButtonProcessItem', 'text-22'],
+        ];
+    }
+
+    /**
+     * @dataProvider addMessageProvider
+     */
+    public function testAddSpecifiedMessages($key, $value)
+    {
+        Yii::$app->getModule('filepond');
         $adapter = new ConfigAdapter([
             'filePond' => [
-                'labelIdle' => 'text-1',
-                'labelInvalidField' => 'text-2',
-                'labelFileWaitingForSize' => 'text-3',
-                'labelFileSizeNotAvailable' => 'text-4',
-                'labelFileLoading' => 'text-5',
-                'labelFileLoadError' => 'text-6',
-                'labelFileProcessing' => 'text-7',
-                'labelFileProcessingComplete' => 'text-8',
-                'labelFileProcessingAborted' => 'text-9',
-                'labelFileProcessingError' => 'text-10',
-                'labelFileProcessingRevertError' => 'text-11',
-                'labelFileRemoveError' => 'text-12',
-                'labelTapToCancel' => 'text-13',
-                'labelTapToRetry' => 'text-14',
-                'labelTapToUndo' => 'text-15',
-                'labelButtonRemoveItem' => 'text-16',
-                'labelButtonAbortItemLoad' => 'text-17',
-                'labelButtonRetryItemLoad' => 'text-18',
-                'labelButtonAbortItemProcessing' => 'text-19',
-                'labelButtonUndoItemProcessing' => 'text-20',
-                'labelButtonRetryItemProcessing' => 'text-21',
-                'labelButtonProcessItem' => 'text-22',
+                $key => $value,
             ],
         ]);
 
         $adapter->addMessages();
-        $this->assertEquals('text-1', $adapter->filePond['labelIdle']);
-        $this->assertEquals('text-2', $adapter->filePond['labelInvalidField']);
-        $this->assertEquals('text-3', $adapter->filePond['labelFileWaitingForSize']);
-        $this->assertEquals('text-4', $adapter->filePond['labelFileSizeNotAvailable']);
-        $this->assertEquals('text-5', $adapter->filePond['labelFileLoading']);
-        $this->assertEquals('text-6', $adapter->filePond['labelFileLoadError']);
-        $this->assertEquals('text-7', $adapter->filePond['labelFileProcessing']);
-        $this->assertEquals('text-8', $adapter->filePond['labelFileProcessingComplete']);
-        $this->assertEquals('text-9', $adapter->filePond['labelFileProcessingAborted']);
-        $this->assertEquals('text-10', $adapter->filePond['labelFileProcessingError']);
-        $this->assertEquals('text-11', $adapter->filePond['labelFileProcessingRevertError']);
-        $this->assertEquals('text-12', $adapter->filePond['labelFileRemoveError']);
-        $this->assertEquals('text-13', $adapter->filePond['labelTapToCancel']);
-        $this->assertEquals('text-14', $adapter->filePond['labelTapToRetry']);
-        $this->assertEquals('text-15', $adapter->filePond['labelTapToUndo']);
-        $this->assertEquals('text-16', $adapter->filePond['labelButtonRemoveItem']);
-        $this->assertEquals('text-17', $adapter->filePond['labelButtonAbortItemLoad']);
-        $this->assertEquals('text-18', $adapter->filePond['labelButtonRetryItemLoad']);
-        $this->assertEquals('text-19', $adapter->filePond['labelButtonAbortItemProcessing']);
-        $this->assertEquals('text-20', $adapter->filePond['labelButtonUndoItemProcessing']);
-        $this->assertEquals('text-21', $adapter->filePond['labelButtonRetryItemProcessing']);
-        $this->assertEquals('text-22', $adapter->filePond['labelButtonProcessItem']);
+        $this->assertEquals($value, $adapter->filePond[$key]);
+    }
 
+    /**
+     * @dataProvider addMessageProvider
+     */
+    public function testAddDefaultMessages($key, $value)
+    {
         Yii::$app->getModule('filepond');
         $adapter = new ConfigAdapter();
         $adapter->addMessages();
-        $this->assertArrayHasKey('labelIdle', $adapter->filePond);
-        $this->assertArrayHasKey('labelInvalidField', $adapter->filePond);
-        $this->assertArrayHasKey('labelFileWaitingForSize', $adapter->filePond);
-        $this->assertArrayHasKey('labelFileSizeNotAvailable', $adapter->filePond);
-        $this->assertArrayHasKey('labelFileLoading', $adapter->filePond);
-        $this->assertArrayHasKey('labelFileLoadError', $adapter->filePond);
-        $this->assertArrayHasKey('labelFileProcessing', $adapter->filePond);
-        $this->assertArrayHasKey('labelFileProcessingComplete', $adapter->filePond);
-        $this->assertArrayHasKey('labelFileProcessingAborted', $adapter->filePond);
-        $this->assertArrayHasKey('labelFileProcessingError', $adapter->filePond);
-        $this->assertArrayHasKey('labelFileProcessingRevertError', $adapter->filePond);
-        $this->assertArrayHasKey('labelFileRemoveError', $adapter->filePond);
-        $this->assertArrayHasKey('labelTapToCancel', $adapter->filePond);
-        $this->assertArrayHasKey('labelTapToRetry', $adapter->filePond);
-        $this->assertArrayHasKey('labelTapToUndo', $adapter->filePond);
-        $this->assertArrayHasKey('labelButtonRemoveItem', $adapter->filePond);
-        $this->assertArrayHasKey('labelButtonAbortItemLoad', $adapter->filePond);
-        $this->assertArrayHasKey('labelButtonRetryItemLoad', $adapter->filePond);
-        $this->assertArrayHasKey('labelButtonAbortItemProcessing', $adapter->filePond);
-        $this->assertArrayHasKey('labelButtonUndoItemProcessing', $adapter->filePond);
-        $this->assertArrayHasKey('labelButtonRetryItemProcessing', $adapter->filePond);
-        $this->assertArrayHasKey('labelButtonProcessItem', $adapter->filePond);
+        $this->assertArrayHasKey($key, $adapter->filePond);
     }
 }
